f12be9dcef6ee776454326b23fc484af80fe1c1b
cmwl <1357252390@qq.com>
2020-11-07 11:43:52 +0800

    入库

--- app/Http/Controllers/Hfive/HfiveController.php
+++ app/Http/Controllers/Hfive/HfiveController.php
@@ -5,25 +5,22 @@ namespace App\Http\Controllers\Hfive;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
+use App\Model\User;
 class HfiveController extends Controller
 {
     public function hfive()
     {
 
         if ($this->checkSignature()) {
-            // $access_token=$this->get_access_token();  //跳方法  调 access_token  获取access_token
             $str = file_get_contents("php://input");
             $obj = simplexml_load_string($str, "SimpleXMLElement", LIBXML_NOCDATA);
-            // $obj=json_decode($obj, true);
-            // file_put_contents("aaa.txt",$obj);
-            // echo "ok";
             switch ($obj->MsgType) {
                 case 'event':
                     if ($obj->Event == "subscribe") {
                         //用户扫码的 openID
                         $openid = $obj->FromUserName;//获取发送方的 openid
-                        $access_token = $this->get_access_token();//获取token,
-                        $url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=" . $access_token . "&openid=" . $openid . "&lang=zh_CN";
+                        $access_token = $this->assecc_token();//获取token,
+                        $url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=".$access_token."&openid=".$openid."&lang=zh_CN";
                         //掉接口
                         $user = json_decode($this->http_get($url), true);//跳方法 用get  方式调第三方类库
                         // $this->writeLog($fens);
@@ -31,16 +28,14 @@ class HfiveController extends Controller
                             $this->writeLog("获取用户信息失败");
                         } else {
                             //说明查找成功 //可以加入数据库
-
+                             User::insert($user);
                             $content = "您好!感谢您的关注";
                         }
                     }
                     if ($obj->Event == "unsubscribe") {
                         $content = "取消关注成功,期待您下次关注";
-
                     }
                     break;
-
             }
             echo $this->xiaoxi($obj, $content);
         }
