334680ff809cbf641cdeef6dfad42b423a359aa7
cmwl <1357252390@qq.com>
2020-11-08 16:08:30 +0800

    tijiao

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -15,8 +15,38 @@ class IndexController extends HfiveController
        $url = "http://apis.juhe.cn/simpleWeather/query?city=".$city."&key=".$key;
        $user = json_decode($this->http_get($url), true);//跳方法 用get  方式调第三方类库
 
-       $content = $user['result']['city']."天气情况:"."\r\n"."天气:".$user['result']['realtime']['info']."\r\n"."温度:".$user['result']['realtime']['temperature']."\r\n"."湿度:".$user['result']['realtime']['humidity']."\r\n"."风向:".$user['result']['realtime']['direct']."\r\n"."风力:".$user['result']['realtime']['power']."\r\n"."空气质量:".$user['result']['realtime']['aqi'];
-       dd($user['reason']);
+       $content = $user['result']['city']."天气情况:".
+           "\r\n"."天气:".$user['result']['realtime']['info'].
+           "\r\n"."温度:".$user['result']['realtime']['temperature'].
+           "\r\n"."湿度:".$user['result']['realtime']['humidity'].
+           "\r\n"."风向:".$user['result']['realtime']['direct'].
+           "\r\n"."风力:".$user['result']['realtime']['power'].
+           "\r\n"."空气质量:".$user['result']['realtime']['aqi'].
+           "\r\n"."近五天天气情况如下:".
+           "\r\n".$user['result']['future'][0]['date'].":".
+           "\r\n"."天气:".$user['result']['future'][0]['weather'].
+           "\r\n"."温度:".$user['result']['future'][0]['temperature'].
+           "\r\n"."风向:".$user['result']['future'][0]['direct'].
+           "\r\n".$user['result']['future'][1]['date'].":".
+           "\r\n"."天气:".$user['result']['future'][1]['weather'].
+           "\r\n"."温度:".$user['result']['future'][1]['temperature'].
+           "\r\n"."风向:".$user['result']['future'][1]['direct'].
+           "\r\n".$user['result']['future'][2]['date'].":".
+           "\r\n"."天气:".$user['result']['future'][2]['weather'].
+           "\r\n"."温度:".$user['result']['future'][2]['temperature'].
+           "\r\n"."风向:".$user['result']['future'][2]['direct'].
+           "\r\n".$user['result']['future'][3]['date'].":".
+           "\r\n"."天气:".$user['result']['future'][3]['weather'].
+           "\r\n"."温度:".$user['result']['future'][3]['temperature'].
+           "\r\n"."风向:".$user['result']['future'][3]['direct'].
+           "\r\n".$user['result']['future'][4]['date'].":".
+           "\r\n"."天气:".$user['result']['future'][4]['weather'].
+           "\r\n"."温度:".$user['result']['future'][4]['temperature'].
+           "\r\n"."风向:".$user['result']['future'][4]['direct'];
+
+
+
+       dd($content);
        exit;
        $a="<xml><ToUserName><![CDATA[gh_2bdc7cc9336f]]></ToUserName>
               <FromUserName><![CDATA[oM539vhyM4XQe1cp194eOWPJZl6M]]></FromUserName>
