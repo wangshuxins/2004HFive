09faaa8be9cb4b2a656fc43c6e2972a040e98c81
cmwl <1357252390@qq.com>
2020-11-11 17:42:32 +0800

    提交

--- app/Http/Controllers/Hfive/HfiveController.php
+++ app/Http/Controllers/Hfive/HfiveController.php
@@ -83,7 +83,7 @@ class HfiveController extends Controller
 
 						 if($obj->EventKey=="wx_521"){
                               $key = $obj->FromUserName;
-							  $times = date("Y-m-d",time());
+							  $times = "2020-11-12";
                               $date = Redis::zrange($key,0,-1);
 							  if($date){
 							      $date = $date[0];
