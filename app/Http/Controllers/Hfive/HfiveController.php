1379decd92c7942aa194d8fc02368bada1271c3e
cmwl <1357252390@qq.com>
2020-11-11 12:38:08 +0800

    提交

--- app/Http/Controllers/Hfive/HfiveController.php
+++ app/Http/Controllers/Hfive/HfiveController.php
@@ -82,10 +82,10 @@ class HfiveController extends Controller
 					 if ($obj->Event == "CLICK") {
 
 						 if($obj->EventKey=="wx_521"){
-                             $openid = $obj->FromUserName;
-							 $key = Redis::zadd($openid,0,1);
+                             $key = $obj->FromUserName;
+							 Redis::zadd($key,0,1);
 						     $zincrby = Redis::zincrby($key,1,1);
-						    $content="签到成功您以签到".$zincrby."天";
+						    $content="签到成功您以积累签到".$zincrby."天";
 						 }else{
 						 
 						    $city =  urlencode("北京");
