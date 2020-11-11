e1fcd95c3f1f72d315191966637ce6d50ed078cb
cmwl <1357252390@qq.com>
2020-11-11 15:49:22 +0800

    提交

--- app/Http/Controllers/Hfive/HfiveController.php
+++ app/Http/Controllers/Hfive/HfiveController.php
@@ -86,7 +86,7 @@ class HfiveController extends Controller
 							  $times = date("Y-m-d",time());
                               $date = Redis::zrange($key,0,-1);
 							
-						       if($date==$times){	
+						       if(1==1){	
 									 $content = "您今日以及签到过了!";
 								 }else{
 									 $zincrby = Redis::zincrby($key,1,$times);
