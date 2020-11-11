3d51d7fb0ceedea7ea0519f2bee8e1ea2a09f39a
cmwl <1357252390@qq.com>
2020-11-11 13:47:42 +0800

    提交

--- app/Http/Controllers/Hfive/HfiveController.php
+++ app/Http/Controllers/Hfive/HfiveController.php
@@ -85,9 +85,8 @@ class HfiveController extends Controller
                               $key = $obj->FromUserName;
 							  $zincrby = Redis::zincrby($key,1,1);
 							  $zadd = Redis::zadd($key,$zincrby,1);
-							  $a = array_xml($key);
-                              $keys = $a['FromUserName'];
-							  Redis::set($key."s",$keys);
+							 
+							  Redis::set($key."s",$key);
 							  
 						      $content="签到成功您以积累签到".$zincrby."天";
 						 }else{
